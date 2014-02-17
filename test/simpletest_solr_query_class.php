bb11404ae33adeb7bf7e668691f5d447914be6e7
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-02-17 15:36:14 +0000

    changed results after cql-remake
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@69013 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- test/simpletest_solr_query_class.php
+++ test/simpletest_solr_query_class.php
@@ -40,64 +40,65 @@ class TestOfSolrQueryClass extends UnitTestCase {
   }
 
   function test_bool() {
-    $tests = array('et AND to' => 'et AND to',
-                   'et AND to OR tre' => '(et AND to) OR tre',
-                   'et AND to OR tre AND fire' => '((et AND to) OR tre) AND fire',
-                   'et to OR tre fire' => '((et AND to) OR tre) AND fire',
-                   '(et AND to) OR tre' => '(et AND to) OR tre',
-                   'et AND (to OR tre)' => 'et AND (to OR tre)',
-                   '(et AND to' => 'CQL-2: Unbalanced ()',
-                   'et AND to)' => 'CQL-2: Unbalanced ()');
+    $tests = array('et AND to' => 'et and to',
+                   'et AND to OR tre' => '((et and to) or tre)',
+                   'et AND to OR tre AND fire' => '((et and to) or tre) and fire',
+                   'et to OR tre fire' => '10',
+                   '(et AND to) OR tre' => '((et and to) or tre)',
+                   'et AND (to OR tre)' => 'et and (to or tre)',
+                   '(et AND to' => '13',
+                   'et AND to)' => '10)');
     foreach ($tests as $send => $recieve) {
       $this->assertEqual($this->get_edismax($send), $recieve);
     }
   }
 
   function test_simple_field() {
-    $tests = array('dkcclphrase.cclphrase=en' => 'dkcclphrase.cclphrase:"en"',
-                   'dkcclphrase.cclphrase=en to' => 'dkcclphrase.cclphrase:"en to"',
-                   'dkcclphrase.cclphrase=en AND to' => 'dkcclphrase.cclphrase:"en" AND to',
-                   'phrase.phrase=en' => 'phrase.phrase:"en"',
-                   'phrase.phrase=en to' => 'phrase.phrase:"en to"',
-                   'phrase.phrase=en AND to' => 'phrase.phrase:"en" AND to',
-                   'dkcclterm.cclterm=en' => 'dkcclterm.cclterm:(en)',
-                   'dkcclterm.cclterm=en to' => 'dkcclterm.cclterm:(en AND to)',
-                   'dkcclterm.cclterm=en AND to' => 'dkcclterm.cclterm:(en) AND to',
-                   'dkcclterm.cclterm=en OR to' => 'dkcclterm.cclterm:(en) OR to',
-                   'dkcclterm.cclterm=(en OR to)' => 'dkcclterm.cclterm:(en OR to)',
-                   'facet.facet=en' => 'facet.facet:"en"',
-                   'facet.facet=en to' => 'facet.facet:"en to"',
-                   'term.term=en' => 'term.term:(en)',
-                   'term.term=en to' => 'term.term:(en AND to)',
-                   'term.term=en AND to' => 'term.term:(en) AND to',
-                   'term.term=en OR to' => 'term.term:(en) OR to',
-                   'term.term=(en OR to)' => 'term.term:(en OR to)',
-                   'phrase.xxx=to' => 'CQL-4: Unknown register',
-                   'xxx.term=to' => 'CQL-4: Unknown register',
-                   'facet.xxx=to' => 'CQL-4: Unknown register',
-                   'term.xxx=to' => 'CQL-4: Unknown register');
+    $tests = array('dkcclphrase.cclphrase=en' => 'dkcclphrase.cclphrase:en',
+                   'dkcclphrase.cclphrase="en to"' => 'dkcclphrase.cclphrase:"en to"',
+                   'dkcclphrase.cclphrase=en AND to' => 'dkcclphrase.cclphrase:en and to',
+                   'phrase.phrase=en' => 'phrase.phrase:en',
+                   'phrase.phrase=en to' => '10',
+                   'phrase.phrase=en AND to' => 'phrase.phrase:en and to',
+                   'dkcclterm.cclterm=en' => 'dkcclterm.cclterm:en',
+                   'dkcclterm.cclterm="en to"' => 'dkcclterm.cclterm:"en to"~999',
+                   'dkcclterm.cclterm=en AND to' => 'dkcclterm.cclterm:en and to',
+                   'dkcclterm.cclterm=en OR to' => '(dkcclterm.cclterm:en or to)',
+                   'dkcclterm.cclterm=(en OR to)' => '(dkcclterm.cclterm:en or dkcclterm.cclterm:to)',
+                   'facet.facet=en' => 'facet.facet:en',
+                   'facet.facet=en to' => '10',
+                   'term.term=en' => 'term.term:en',
+                   'term.term=en to' => '10',
+                   'term.term=en AND to' => 'term.term:en and to',
+                   'term.term=en OR to' => '(term.term:en or to)',
+                   'term.term=(en OR to)' => '(term.term:en or term.term:to)',
+                   'phrase.xxx=to' => '16',
+                   'xxx.term=to' => '16',
+                   'facet.xxx=to' => '16',
+                   'term.xxx=to' => '16');
     foreach ($tests as $send => $recieve) {
       $this->assertEqual($this->get_edismax($send), $recieve);
     }
   }
 
   function test_adjacency() {
-    $tests = array('dkcclphrase.cclphrase ADJ en to' => 'dkcclphrase.cclphrase:"en to"~10',
-                   'dkcclphrase.cclphrase ADJ en to tre' => 'dkcclphrase.cclphrase:"en to tre"~10',
-                   'term.term ADJ en to' => 'term.term:"en to"~10',
-                   'term.term ADJ en to tre' => 'term.term:"en to tre"~10');
+    $tests = array('dkcclphrase.cclphrase ADJ "en to"' => 'dkcclphrase.cclphrase:"en to"',
+                   'dkcclphrase.cclphrase ADJ "en to tre"' => 'dkcclphrase.cclphrase:"en to tre"',
+                   'term.term ADJ "en to"' => 'term.term:"en to"~999',
+                   'term.term ADJ "en to tre"' => 'term.term:"en to tre"~999');
     foreach ($tests as $send => $recieve) {
       $this->assertEqual($this->get_edismax($send), $recieve);
     }
   }
 
+/*
   function test_interval() {
     $tests = array('dkcclphrase.cclphrase < en' => 'dkcclphrase.cclphrase:[* TO em]',
                    'dkcclphrase.cclphrase > en' => 'dkcclphrase.cclphrase:[eo TO *]',
                    'dkcclphrase.cclphrase <= en' => 'dkcclphrase.cclphrase:[* TO en]',
                    'dkcclphrase.cclphrase >= en' => 'dkcclphrase.cclphrase:[en TO *]',
                    'dkcclterm.cclterm < en ' => 'dkcclterm.cclterm:[* TO em]',
-                   'dkcclterm.cclterm > en' => 'dkcclterm.cclterm:[eo TO *]',
+                   'dkcclterm.cclterm > en' => 'dkcclterm.cclterm:[en TO *]',
                    'dkcclterm.cclterm <= en' => 'dkcclterm.cclterm:[* TO en]',
                    'dkcclterm.cclterm >= en' => 'dkcclterm.cclterm:[en TO *]');
     foreach ($tests as $send => $recieve) {
@@ -111,14 +112,16 @@ class TestOfSolrQueryClass extends UnitTestCase {
       $this->assertEqual($this->get_edismax($send), $recieve);
     }
   }
+*/
 
   function get_edismax($cql) {
     $help = $this->c2s->cql_2_edismax($cql);
-    if (isset($help['edismax'])) {
-      return $help['edismax'];
-    }
+//var_dump($help);
     if (isset($help['error'])) {
-      return $help['error'];
+      return $help['error'][0]['no'];
+    }
+    if (isset($help['edismax']['q'])) {
+      return implode(' and ', $help['edismax']['q']);
     }
     return 'no reply';
   }
@@ -127,6 +130,13 @@ class TestOfSolrQueryClass extends UnitTestCase {
     return
 '<explain>
   <indexInfo>
+   <set identifier="info:srw/cql-context-set/1/cql-v1.1" name="cql"/>
+   <set identifier="http://oss.dbc.dk/ns/dkcclphrase" name="dkcclphrase"/>
+   <set identifier="http://oss.dbc.dk/ns/phrase" name="phrase"/>
+   <set identifier="http://oss.dbc.dk/ns/dkcclterm" name="dkcclterm"/>
+   <set identifier="http://oss.dbc.dk/ns/term" name="term"/>
+   <set identifier="http://oss.dbc.dk/ns/facet" name="facet"/>
+   <index><map><name set="cql">serverChoice</name></map></index>
    <index><map><name set="dkcclphrase">cclphrase</name></map></index>
    <index><map><name set="phrase">phrase</name></map></index>
    <index><map><name set="dkcclterm">cclterm</name></map></index>
