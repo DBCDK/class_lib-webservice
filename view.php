550f41691924919cc28dee34b7eb34677722438d
hhl <hhl@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-09-12 11:52:31 +0000

    lagt view i OLS
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@76098 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

---
+++ view.php
@@ -0,0 +1,30 @@
+<?php
+class view {
+  private $templateFileName ="";
+  private $parameters = array();
+  private $parsed = false;
+  
+  function __construct($template="") {
+    if (empty($template)) 
+      $template = basename($_SERVER['SCRIPT_FILENAME'], 'php') . 'thtml';
+    $this->templateFileName = $template;
+    }
+    
+  function __destruct() {
+    if (!$this->parsed)
+      $this->parse();
+  }
+  
+  public function set($var, $value) {
+    $this->parameters[$var] = $value;
+  }
+  
+  public function parse() {
+    foreach ($this->parameters as $parameter_variable_name => $parameter_value)
+      $$parameter_variable_name = $parameter_value;
+    unset($parameter_variable_name, $parameter_value);  // Do not expose these variables to the template
+    require($this->templateFileName);
+    $this->parsed = true;
+  }
+}
+?>
