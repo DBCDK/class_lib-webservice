87ba5d346f2bd8f2c67bb51a4853d608f2b68636
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-09-17 09:13:39 +0000

    full suite
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@76320 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- test/simpletest_cql2tree_class.php
+++ test/simpletest_cql2tree_class.php
@@ -8,6 +8,8 @@ require_once('cql2tree_class.php');
 
 class TestOfCql2TreeClass extends UnitTestCase {
   private $c2t;
+  private $cqlns;
+  private $indexes;
 
   function __construct() {
     parent::__construct();
@@ -24,17 +26,16 @@ class TestOfCql2TreeClass extends UnitTestCase {
   }
 
   function test_basal() {
-    $query = 'test';
-    $this->c2t->parse($query);
-    $tree = $this->c2t->result();
+    $tree = self::get_tree('test');
     $this->assertEqual($tree['type'], 'searchClause');
     $this->assertEqual($tree['term'], 'test');
+    $this->assertEqual($tree['field'], 'serverChoice');
+    $this->assertEqual($tree['prefix'], 'cql');
+    $this->assertEqual($tree['relation'], 'scr');
   }
 
   function test_bool() {
-    $query = 'test and some';
-    $this->c2t->parse($query);
-    $tree = $this->c2t->result();
+    $tree = self::get_tree('test and some');
     $this->assertEqual($tree['type'], 'boolean');
     $this->assertEqual($tree['op'], 'and');
     $this->assertEqual($tree['left']['term'], 'test');
@@ -42,54 +43,118 @@ class TestOfCql2TreeClass extends UnitTestCase {
   }
 
   function test_simple_field() {
+    $tree = self::get_tree('dkcclphrase.cclphrase=test');
+    $this->assertEqual($tree['type'], 'searchClause');
+    $this->assertEqual($tree['term'], 'test');
+    $this->assertEqual($tree['field'], 'cclphrase');
+    $this->assertEqual($tree['prefix'], 'dkcclphrase');
+    $this->assertEqual($tree['fielduri'], $this->cqlns['dkcclphrase']);
+    $this->assertEqual($tree['relation'], '=');
   }
 
   function test_adjacency() {
+    $tree = self::get_tree('dkcclphrase.cclphrase ADJ "test some"');
+    $this->assertEqual($tree['type'], 'searchClause');
+    $this->assertEqual($tree['term'], 'test some');
+    $this->assertEqual($tree['field'], 'cclphrase');
+    $this->assertEqual($tree['prefix'], 'dkcclphrase');
+    $this->assertEqual($tree['fielduri'], $this->cqlns['dkcclphrase']);
+    $this->assertEqual($tree['relation'], 'ADJ');
   }
 
   function test_interval() {
+    $tree = self::get_tree('dkcclphrase.cclphrase<test');
+    $this->assertEqual($tree['type'], 'searchClause');
+    $this->assertEqual($tree['term'], 'test');
+    $this->assertEqual($tree['field'], 'cclphrase');
+    $this->assertEqual($tree['prefix'], 'dkcclphrase');
+    $this->assertEqual($tree['fielduri'], $this->cqlns['dkcclphrase']);
+    $this->assertEqual($tree['relation'], '<');
   }
 
   function test_complex() {
+    $tree = self::get_tree('facet.facet="karen blixen" AND term.term=bog');
+    $this->assertEqual($tree['type'], 'boolean');
+    $this->assertEqual($tree['op'], 'and');
+    $this->assertEqual($tree['left']['type'], 'searchClause');
+    $this->assertEqual($tree['left']['term'], 'karen blixen');
+    $this->assertEqual($tree['left']['field'], 'facet');
+    $this->assertEqual($tree['left']['prefix'], 'facet');
+    $this->assertEqual($tree['left']['fielduri'], $this->cqlns['facet']);
+    $this->assertEqual($tree['left']['relation'], '=');
+    $this->assertEqual($tree['right']['type'], 'searchClause');
+    $this->assertEqual($tree['right']['term'], 'bog');
+    $this->assertEqual($tree['right']['field'], 'term');
+    $this->assertEqual($tree['right']['prefix'], 'term');
+    $this->assertEqual($tree['right']['fielduri'], $this->cqlns['term']);
+    $this->assertEqual($tree['right']['relation'], '=');
   }
 
   function test_slop() {
+    $tree = self::get_tree('term.slop="karen blixen"');
+    $this->assertEqual($tree['type'], 'searchClause');
+    $this->assertEqual($tree['term'], 'karen blixen');
+    $this->assertEqual($tree['slop'], 10);
+    $tree = self::get_tree('term.term="karen blixen"');
+    $this->assertEqual($tree['type'], 'searchClause');
+    $this->assertEqual($tree['term'], 'karen blixen');
+    $this->assertEqual($tree['slop'], 9999);
   }
 
   function test_alias() {
-  }
-
-  function test_filter() {
-  }
-
-  function test_trunkation() {
-  }
-
-  function test_masking() {
+    $tree = self::get_tree('slop="karen blixen"');
+    $this->assertEqual($tree['type'], 'searchClause');
+    $this->assertEqual($tree['term'], 'karen blixen');
+    $this->assertEqual($tree['slop'], 5);
+    $this->assertEqual($tree['field'], 'slop');
+    $this->assertEqual($tree['prefix'], 'term');
+    $this->assertEqual($tree['fielduri'], $this->cqlns['term']);
   }
 
   function test_errors() {
+    $diags = self::get_diagnostics('en prox/unit=word/distance=2 to');
+    $this->assertEqual($diags[0]['no'], 37);
+    $this->assertEqual($diags[0]['details'], 'prox');
+    $this->assertEqual($diags[0]['pos'], '7');
+    $diags = self::get_diagnostics('ttt.xxx=test');
+    $this->assertEqual($diags[0]['no'], 16);
+    $this->assertEqual($diags[0]['details'], 'ttt.xxx');
+    $this->assertEqual($diags[0]['pos'], '12');
+  }
+
+  function get_tree($query) {
+    $this->c2t->parse($query);
+    return $this->c2t->result();
+  }
+
+  function get_diagnostics($query) {
+    $this->c2t->parse($query);
+    return $this->c2t->get_diagnostics();
   }
 
   function cqlns() {
-    return array('cql' => 'info:srw/cql-context-set/1/cql-v1.1',
-                 'dkcclphrase' => 'http://oss.dbc.dk/ns/dkcclphrase',
-                 'phrase' => 'http://oss.dbc.dk/ns/phrase',
-                 'dkcclterm' => 'http://oss.dbc.dk/ns/dkcclterm',
-                 'term' => 'http://oss.dbc.dk/ns/term',
-                 'facet' => 'http://oss.dbc.dk/ns/facet');
+    $this->cqlns = 
+      array('cql' => 'info:srw/cql-context-set/1/cql-v1.1',
+            'dkcclphrase' => 'http://oss.dbc.dk/ns/dkcclphrase',
+            'phrase' => 'http://oss.dbc.dk/ns/phrase',
+            'dkcclterm' => 'http://oss.dbc.dk/ns/dkcclterm',
+            'term' => 'http://oss.dbc.dk/ns/term',
+            'facet' => 'http://oss.dbc.dk/ns/facet');
+    return $this->cqlns;
   }
   
   function indexes() {
-    return array('serverChoice' => array('cql' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
-                 'cclphrase' => array('dkcclphrase' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
-                 'phrase' => array('phrase' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
-                 'cclterm' => array('dkcclterm' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
-                 'filter' => array('term' => array('filter' => true, 'slop' => 9999, 'handler' => '')),
-                 'term' => array('term' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
-                 'slop' => array('term' => array('filter' => false, 'slop' => 10, 'handler' => ''),
-                                 '' => array('alias' => array('slop' => 5, 'handler' => '', 'prefix' => 'term', 'field' => 'slop'))),
-                 'facet' => array('facet' => array('filter' => false, 'slop' => 9999, 'handler' => '')));
+    $this->indexes = 
+      array('serverChoice' => array('cql' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
+            'cclphrase' => array('dkcclphrase' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
+            'phrase' => array('phrase' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
+            'cclterm' => array('dkcclterm' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
+            'filter' => array('term' => array('filter' => true, 'slop' => 9999, 'handler' => '')),
+            'term' => array('term' => array('filter' => false, 'slop' => 9999, 'handler' => '')),
+            'slop' => array('term' => array('filter' => false, 'slop' => 10, 'handler' => ''),
+                            '' => array('alias' => array('slop' => 5, 'handler' => '', 'prefix' => 'term', 'field' => 'slop'))),
+            'facet' => array('facet' => array('filter' => false, 'slop' => 9999, 'handler' => '')));
+    return $this->indexes;
   }
 
 }
