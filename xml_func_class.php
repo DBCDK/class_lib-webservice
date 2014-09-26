e24004a5c4cfe28dbecf6449421c0710d334f81c
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-09-26 10:37:57 +0000

    formatting
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@76827 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- xml_func_class.php
+++ xml_func_class.php
@@ -29,54 +29,53 @@ require_once("xmlwriter_class.php");
   $xml = xml_func::object_to_xml($object);
 
 */
-class xml_func
-{ 
+class xml_func { 
   /* return an object as xml */
-  public static function object_to_xml($obj)
-  {
+  public static function object_to_xml($obj) {
     global $key;
     global $xmlwriter;
 
-    if( !isset($xmlwriter) )
+    if (! isset($xmlwriter)) {
       $xmlwriter=new XmlWrite();
+    }
     
-    if( is_object($obj) )
-      {
-	$xmlwriter->push(get_class($obj));
-	$vars=get_object_vars($obj);
-	foreach( $vars as $key=>$var )
-	  {
-	    if( is_scalar($var) )
-	      $xmlwriter->element($key,$var);
-	    else
-	      self::object_to_xml($var);	      
-	  }
-	$xmlwriter->pop();
+    if (is_object($obj)) {
+      $xmlwriter->push(get_class($obj));
+      $vars=get_object_vars($obj);
+      foreach ($vars as $key=>$var) {
+        if (is_scalar($var)) {
+          $xmlwriter->element($key,$var);
+        }
+        else {
+          self::object_to_xml($var);        
+        }
       }
-    else if( is_array($obj) )
-      {
-	foreach( $obj as $whatever=>$val )
-	  if( is_scalar($val) )
-	    $xmlwriter->element($key,$val);
-	  else
-	    self::object_to_xml($val);
+      $xmlwriter->pop();
+    }
+    elseif (is_array($obj)) {
+      foreach ($obj as $whatever=>$val) {
+        if (is_scalar($val)) {
+          $xmlwriter->element($key,$val);
+        }
+        else {
+          self::object_to_xml($val);
+        }
       }
-    
+    }
+
     return $xmlwriter->getXml();
   } 
 
   /* fix UTF8-encoding */
-  public static function UTF8($data)
-  {
+  public static function UTF8($data) {
     $encoding = mb_detect_encoding($data) ;
-    if($encoding == "UTF-8" && mb_check_encoding($data,"UTF-8"))
-      {
-	//hmm . '&' doesn't encode properly TODO find a proper fix
-	return str_ireplace('&','&#38;',$data);
-      }
-    else
+    if ($encoding == "UTF-8" && mb_check_encoding($data,"UTF-8")) {
+      //hmm . '&' doesn't encode properly TODO find a proper fix
+      return str_ireplace('&','&#38;',$data);
+    }
+    else {
       return  str_ireplace('&','&#38;',utf8_encode($data)); 
+    }
   }
 
 }
-?>
