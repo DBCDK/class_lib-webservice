f26360b7d1b891346c39a88a68e542e72e054606
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-10-29 07:57:43 +0000

    rename pg_test to pg_simple_test
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@78441 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- test/simpletest_pg_database_class.php
+++ test/simpletest_pg_database_class.php
@@ -40,71 +40,71 @@ class TestOfPgDatabaseClass extends UnitTestCase {
   }
 
   function test_create() {
-    $this->assertEqual(self::do_sql('DROP TABLE pg_test'), 'ERROR:  table "pg_test" does not exist');
-    $this->assertEqual(self::do_sql('CREATE TABLE pg_test (tal integer, str varchar(40))'), 'ok');
+    $this->assertEqual(self::do_sql('DROP TABLE pg_simple_test'), 'ERROR:  table "pg_simple_test" does not exist');
+    $this->assertEqual(self::do_sql('CREATE TABLE pg_simple_test (tal integer, str varchar(40))'), 'ok');
   }
 
   function test_insert() {
     $this->pg->bind('tal', 1);
     $this->pg->bind('str', 'en');
-    $this->assertEqual(self::do_sql("INSERT INTO pg_test VALUES (:tal, :str)"), 'ok');
-    $this->assertEqual(self::do_sql("INSERT INTO pg_test VALUES (2, 'to')"), 'ok');
+    $this->assertEqual(self::do_sql("INSERT INTO pg_simple_test VALUES (:tal, :str)"), 'ok');
+    $this->assertEqual(self::do_sql("INSERT INTO pg_simple_test VALUES (2, 'to')"), 'ok');
   }
 
   function test_select() {
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test WHERE tal = 1'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test WHERE tal = 1'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 1);
     $this->assertEqual($this->pg->get_row(), array('tal' => 1, 'str' => 'en'));
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 2);
     $this->assertEqual($this->pg->get_row(), array('tal' => 1, 'str' => 'en'));
     $this->assertEqual($this->pg->get_row(), array('tal' => 2, 'str' => 'to'));
     $this->assertEqual($this->pg->get_row(), FALSE);
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test'), 'ok');
     $this->assertEqual($this->pg->get_all_rows(), array(array('tal' => 1, 'str' => 'en'), array('tal' => 2, 'str' => 'to')));
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test WHERE tal = 3'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test WHERE tal = 3'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 0);
   }
 
   function test_select_with_bind() {
     $this->pg->bind('tal', 1);
     $this->pg->bind('str', 'en');
-    $this->assertEqual(self::do_sql('SELECT tal FROM pg_test WHERE tal = :tal AND str = :str'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT tal FROM pg_simple_test WHERE tal = :tal AND str = :str'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 1);
     $this->assertEqual($this->pg->get_row(), array('tal' => 1));
     $this->pg->bind('tal', 2);
     $this->pg->bind('str', 'to');
-    $this->assertEqual(self::do_sql('SELECT tal FROM pg_test WHERE tal = :tal AND str = :str'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT tal FROM pg_simple_test WHERE tal = :tal AND str = :str'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 1);
     $this->assertEqual($this->pg->get_row(), array('tal' => 2));
   }
 
   function test_update() {
-    $this->assertEqual(self::do_sql("UPDATE pg_test SET str = 'toto' WHERE tal = 2"), 'ok');
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test WHERE tal = 2'), 'ok');
+    $this->assertEqual(self::do_sql("UPDATE pg_simple_test SET str = 'toto' WHERE tal = 2"), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test WHERE tal = 2'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 1);
     $this->assertEqual($this->pg->get_row(), array('tal' => 2, 'str' => 'toto'));
   }
 
   function test_delete() {
-    $this->assertEqual(self::do_sql('DELETE FROM pg_test WHERE tal = 1'), 'ok');
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test'), 'ok');
+    $this->assertEqual(self::do_sql('DELETE FROM pg_simple_test WHERE tal = 1'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 1);
   }
 
   function test_transaction() {
     $this->pg->start_transaction();
-    $this->assertEqual(self::do_sql("INSERT INTO pg_test VALUES (3, 'tre')"), 'ok');
-    $this->assertEqual(self::do_sql("INSERT INTO pg_test VALUES (4, 'fire')"), 'ok');
+    $this->assertEqual(self::do_sql("INSERT INTO pg_simple_test VALUES (3, 'tre')"), 'ok');
+    $this->assertEqual(self::do_sql("INSERT INTO pg_simple_test VALUES (4, 'fire')"), 'ok');
     $this->pg->end_transaction(FALSE);
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 1);
 
     $this->pg->start_transaction();
-    $this->assertEqual(self::do_sql("INSERT INTO pg_test VALUES (3, 'tre')"), 'ok');
-    $this->assertEqual(self::do_sql("INSERT INTO pg_test VALUES (4, 'fire')"), 'ok');
+    $this->assertEqual(self::do_sql("INSERT INTO pg_simple_test VALUES (3, 'tre')"), 'ok');
+    $this->assertEqual(self::do_sql("INSERT INTO pg_simple_test VALUES (4, 'fire')"), 'ok');
     $this->pg->end_transaction();
-    $this->assertEqual(self::do_sql('SELECT * FROM pg_test'), 'ok');
+    $this->assertEqual(self::do_sql('SELECT * FROM pg_simple_test'), 'ok');
     $this->assertEqual($this->pg->num_rows(), 3);
   }
 
@@ -113,7 +113,7 @@ class TestOfPgDatabaseClass extends UnitTestCase {
   }
 
   function test_cleanup() {
-    $this->assertEqual(self::do_sql('DROP TABLE pg_test'), 'ok');
+    $this->assertEqual(self::do_sql('DROP TABLE pg_simple_test'), 'ok');
   }
 
   function do_sql($sql) {
