37218fcf73a8ea0797075bc77ee38ca66f22cdca
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2014-12-08 13:55:53 +0000

    doxygen
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@79772 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- view.php
+++ view.php
@@ -6,6 +6,8 @@ class view {
     private $parameters = array();
     private $parsed = false;
 
+  /** \brief -
+   */
     function __construct($template = "") {
         if (empty($template))
             $template = basename($_SERVER['SCRIPT_FILENAME'], 'php') . 'thtml';
@@ -17,15 +19,21 @@ class view {
         $this->templateFileName = $template;
     }
 
+  /** \brief -
+   */
     function __destruct() {
         if (!$this->parsed)
             $this->parse();
     }
 
+  /** \brief -
+   */
     public function set($var, $value) {
         $this->parameters[$var] = $value;
     }
 
+  /** \brief -
+   */
     public function parse() {
         foreach ($this->parameters as $parameter_variable_name => $parameter_value)
             $$parameter_variable_name = $parameter_value;
