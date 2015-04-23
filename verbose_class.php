23f6dfc8f18d9f8ec955e9b44c6adeb9b631b706
hhl <hhl@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2015-04-23 11:42:08 +0000

    3rd parameter is made optional
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@84099 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- verbose_class.php
+++ verbose_class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * This file is part of Open Library System.
@@ -17,9 +18,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with Open Library System.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
+ */
 /**
  * \brief Verbose singleton class for loggin to a file or syslog
  *        Use syslog://[facility] as filename for writing to syslog. facility defaults to LOG_LOCAL0
@@ -49,128 +48,133 @@
  * verbose::log(TRACE, 'db::look_up_user()');\n
  *
  * @author Finn Stausgaard - DBC
-**/
-
-@ define('WARNING',0x01);
-@ define('ERROR',0x02);
-@ define('FATAL',0x04);
-@ define('STAT',0x08);
-@ define('TIMER',0x10);
-@ define('DEBUG',0x20);
-@ define('TRACE',0x40);
-@ define('Z3950',0x80);
-@ define('OCI',0x100);
+ * */
+@ define('WARNING', 0x01);
+@ define('ERROR', 0x02);
+@ define('FATAL', 0x04);
+@ define('STAT', 0x08);
+@ define('TIMER', 0x10);
+@ define('DEBUG', 0x20);
+@ define('TRACE', 0x40);
+@ define('Z3950', 0x80);
+@ define('OCI', 0x100);
 
 @ define('SYSLOG_PREFIX', 'syslog://');
 
 class verbose {
 
-  static $verbose_file_name;      ///< -
-  static $syslog_facility = NULL; ///< -
-  static $syslog_id = '';         ///< -
-  static $verbose_mask;           ///< -
-  static $date_format;            ///< -
-  static $my_pid = '';            ///< -
-  static $tracking_id = '';       ///< -
+    static $verbose_file_name;      ///< -
+    static $syslog_facility = NULL; ///< -
+    static $syslog_id = '';         ///< -
+    static $verbose_mask;           ///< -
+    static $date_format;            ///< -
+    static $my_pid = '';            ///< -
+    static $tracking_id = '';       ///< -
 
-  private function __construct() {}
-  private function __destruct() {}
-  private function __clone() {}
+    private function __construct() {
 
-  /**
-   * \brief Sets loglevel and logfile
-   * @param verbose_file_name (string)
-   * @param verbose_mask (string or integer)
-   * @param date_format (string) - format-string for date()
-   **/
-
-  public function open($verbose_file_name, $verbose_mask, $syslog_id, $date_format='') {
-    self::$tracking_id = date('Y-m-d\TH:i:s:') . substr((string)microtime(), 2, 6) . ':' . getmypid();
-    if (!self::$date_format = $date_format)
-      self::$date_format = 'H:i:s-d/m/y';
-    if (strtolower(substr($verbose_file_name, 0, strlen(SYSLOG_PREFIX))) == SYSLOG_PREFIX) {
-      $facility = substr($verbose_file_name, strlen(SYSLOG_PREFIX));     //  syslog://[facility]
-      self::$syslog_facility = defined($facility) ? constant($facility) : LOG_LOCAL0;
-      self::$syslog_id = $syslog_id;
     }
-    self::$verbose_file_name = $verbose_file_name;
-    if (!is_string($verbose_mask)) {
-      self::$verbose_mask = (empty($verbose_mask) ? 0 : $verbose_mask);
+
+    private function __destruct() {
+
     }
-    else {
-      foreach (explode('+', $verbose_mask) as $vm) {
-        if (defined(trim($vm))) self::$verbose_mask |= constant(trim($vm));
-        if ($vm == 'PID') self::$my_pid = ' [' . getmypid() . ']';
-      }
+
+    private function __clone() {
+
     }
-  }
 
-  /**
-   * \brief Logs to a file, or prints out log message.
-   * @param verbose_level Level of verbose output (string)
-   * @param str Log string to write (string)
-   */
-
-  public function log($verbose_level, $str) {
-    if (self::$verbose_file_name && $verbose_level & self::$verbose_mask) {
-      switch ($verbose_level) {
-        case WARNING :
-          $vtext = 'WARNING';
-          break;
-        case ERROR :
-          $vtext = 'ERROR';
-          break;
-        case FATAL :
-          $vtext = 'FATAL';
-          break;
-        case STAT :
-          $vtext = 'STAT';
-          break;
-        case TIMER :
-          $vtext = 'TIMER';
-          break;
-        case DEBUG :
-          $vtext = 'DEBUG';
-          break;
-        case TRACE :
-          $vtext = 'TRACE';
-          break;
-        case Z3950 :
-          $vtext = 'Z3950';
-          break;
-        case OCI :
-          $vtext = 'OCI';
-          break;
-        default :
-          $vtext = 'UNKNOWN';
-          break;
-      }
-
-      if (self::$syslog_facility) {
-       openlog(self::$syslog_id, LOG_ODELAY, self::$syslog_facility);
-       syslog(LOG_INFO, $vtext . ': ' . $str . ' tId:' . self::$tracking_id);
-       closelog();
-      }
-      elseif ($fp = @ fopen(self::$verbose_file_name,'a')) {
-        if (substr($str, strlen($str)-1, 1) <> "\n") $str .= "\n";
-        fwrite($fp, $vtext . self::$my_pid . ' ' . date(self::$date_format) . ' ' . self::$tracking_id . ' ' . $str);
-        fclose($fp);
-      }
-      else
-        die('FATAL: Cannot open ' . self::$verbose_file_name);
+    /**
+     * \brief Sets loglevel and logfile
+     * @param verbose_file_name (string)
+     * @param verbose_mask (string or integer)
+     * @param date_format (string) - format-string for date()
+     * */
+    public function open($verbose_file_name, $verbose_mask, $syslog_id = '', $date_format = '') {
+        self::$tracking_id = date('Y-m-d\TH:i:s:') . substr((string) microtime(), 2, 6) . ':' . getmypid();
+        if (!self::$date_format = $date_format)
+            self::$date_format = 'H:i:s-d/m/y';
+        if (strtolower(substr($verbose_file_name, 0, strlen(SYSLOG_PREFIX))) == SYSLOG_PREFIX) {
+            $facility = substr($verbose_file_name, strlen(SYSLOG_PREFIX));     //  syslog://[facility]
+            self::$syslog_facility = defined($facility) ? constant($facility) : LOG_LOCAL0;
+            self::$syslog_id = $syslog_id;
+        }
+        self::$verbose_file_name = $verbose_file_name;
+        if (!is_string($verbose_mask)) {
+            self::$verbose_mask = (empty($verbose_mask) ? 0 : $verbose_mask);
+        } else {
+            foreach (explode('+', $verbose_mask) as $vm) {
+                if (defined(trim($vm)))
+                    self::$verbose_mask |= constant(trim($vm));
+                if ($vm == 'PID')
+                    self::$my_pid = ' [' . getmypid() . ']';
+            }
+        }
     }
-  }
 
-  /**
-   * \brief Make a unique tracking id
-   * @param t_service_prefix Service prefix that identifies the service
-   * @param t_id Current tracking_id
-   */
+    /**
+     * \brief Logs to a file, or prints out log message.
+     * @param verbose_level Level of verbose output (string)
+     * @param str Log string to write (string)
+     */
+    public function log($verbose_level, $str) {
+        if (self::$verbose_file_name && $verbose_level & self::$verbose_mask) {
+            switch ($verbose_level) {
+                case WARNING :
+                    $vtext = 'WARNING';
+                    break;
+                case ERROR :
+                    $vtext = 'ERROR';
+                    break;
+                case FATAL :
+                    $vtext = 'FATAL';
+                    break;
+                case STAT :
+                    $vtext = 'STAT';
+                    break;
+                case TIMER :
+                    $vtext = 'TIMER';
+                    break;
+                case DEBUG :
+                    $vtext = 'DEBUG';
+                    break;
+                case TRACE :
+                    $vtext = 'TRACE';
+                    break;
+                case Z3950 :
+                    $vtext = 'Z3950';
+                    break;
+                case OCI :
+                    $vtext = 'OCI';
+                    break;
+                default :
+                    $vtext = 'UNKNOWN';
+                    break;
+            }
 
-  public function set_tracking_id($t_service_prefix, $t_id = '') {
-    self::$tracking_id = $t_service_prefix . ':' .  self::$tracking_id .  ($t_id ? '<' . $t_id : '');
-    return self::$tracking_id;
-  }
+            if (self::$syslog_facility) {
+                openlog(self::$syslog_id, LOG_ODELAY, self::$syslog_facility);
+                syslog(LOG_INFO, $vtext . ': ' . $str . ' tId:' . self::$tracking_id);
+                closelog();
+            } elseif ($fp = @ fopen(self::$verbose_file_name, 'a')) {
+                if (substr($str, strlen($str) - 1, 1) <> "\n")
+                    $str .= "\n";
+                fwrite($fp, $vtext . self::$my_pid . ' ' . date(self::$date_format) . ' ' . self::$tracking_id . ' ' . $str);
+                fclose($fp);
+            } else
+                die('FATAL: Cannot open ' . self::$verbose_file_name);
+        }
+    }
+
+    /**
+     * \brief Make a unique tracking id
+     * @param t_service_prefix Service prefix that identifies the service
+     * @param t_id Current tracking_id
+     */
+    public function set_tracking_id($t_service_prefix, $t_id = '') {
+        self::$tracking_id = $t_service_prefix . ':' . self::$tracking_id . ($t_id ? '<' . $t_id : '');
+        return self::$tracking_id;
+    }
 
 }
+
 ?>
