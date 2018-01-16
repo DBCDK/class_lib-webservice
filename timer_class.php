dc8392f1b30bb26f4bb06cd2cd76b8ea31ef40bf
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2018-01-16 13:37:21 +0000

    format
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/class_lib/trunk@118246 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- timer_class.php
+++ timer_class.php
@@ -17,60 +17,59 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with Open Library System.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 
 /** \brief Stopwatch for code timing
-*
-* $watch = new stopwatch();
-* $watch->format('perl');
-*
-* $watch->start('a');
-* $watch->start('w');
-*
-* $watch->stop('a');
-* #$watch->start('a');
-*
-* echo $watch->dump();
-* echo "...\n";
-* $watch->format('screen');
-* echo $watch->dump();
-* echo "...\n";
-*
-* $watch->log("foo.log");
-*
-*/
-
+ *
+ * $watch = new stopwatch();
+ * $watch->format('perl');
+ *
+ * $watch->start('a');
+ * $watch->start('w');
+ *
+ * $watch->stop('a');
+ * #$watch->start('a');
+ *
+ * echo $watch->dump();
+ * echo "...\n";
+ * $watch->format('screen');
+ * echo $watch->dump();
+ * echo "...\n";
+ *
+ * $watch->log("foo.log");
+ *
+ */
 class stopwatch {
-  var $timers;				///< Currently running timers
-  var $sums;				///< Sums of completed timers
-  var $prefix;				///< Prefix of Output
-  var $delim;				///< Delimitor of Output
-  var $postfix;				///< Postfix of Output
-  var $format;				///< Format of Output
+  var $timers;        ///< Currently running timers
+  var $sums;        ///< Sums of completed timers
+  var $prefix;        ///< Prefix of Output
+  var $delim;        ///< Delimitor of Output
+  var $postfix;        ///< Postfix of Output
+  var $format;        ///< Format of Output
 
   /**
-   * \brief constructor
-   * @param $prefix		Output prefix
-   * @param $delim			Output delimitor
-   * @param    $postfix		Output postfix remember newline
-   * @param    $format		Output format ("%s => %01.6f")
+   * \brief stopwatch constructor
+   * @param mixed $prefix Output prefix
+   * @param mixed $delim Output delimitor
+   * @param mixed $postfix Output postfix remember newline
+   * @param mixed $format Output format ("%s => %01.6f")
    *************/
   function __construct($prefix = null, $delim = null, $postfix = null, $format = null) {
-    $this->prefix  = $prefix;
-    $this->delim   = $delim;
+    $this->prefix = $prefix;
+    $this->delim = $delim;
     $this->postfix = $postfix;
-    $this->format  = $format;
-    $this->timers  = Array();
-    $this->sums    = Array();
+    $this->format = $format;
+    $this->timers = [];
+    $this->sums = [];
     $this->start('Total');
   }
 
   /**
-   * 	\brief start a timer
-   *  @param   $s		Name of timer to start
-   *  @param   $ignore (bool)	Ignore already started timer (default true)
-   *************/
+   *  \brief start a timer
+   * @param string $s - Name of timer to start
+   * @param boolean $ignore - Ignore already started timer (default true)
+   */
   function start($s, $ignore = TRUE) {
     if (!isset($this->timers[$s])) {
       $this->timers[$s] = microtime();
@@ -82,13 +81,13 @@ class stopwatch {
   }
 
   /**
-  * \brief stop a timer
-   * @param    $s		Name of timer to stop
-   * @param    $ignore (bool)	Ignore not running timer (default true)
-   *************/
+   * \brief stop a timer
+   * @param string $s - Name of timer to stop
+   * @param boolean $ignore - Ignore not running timer (default true)
+   */
   function stop($s, $ignore = TRUE) {
     if (isset($this->timers[$s])) {
-      list($usec_stop,  $sec_stop) = explode(" ", microtime());
+      list($usec_stop, $sec_stop) = explode(" ", microtime());
       list($usec_start, $sec_start) = explode(" ", $this->timers[$s]);
       $this->timers[$s] = null;
       $this->sums[$s] += ((float)$usec_stop - (float)$usec_start) + (float)($sec_stop - $sec_start);
@@ -100,50 +99,52 @@ class stopwatch {
 
 
   /**
-  *  \brief splittime
-   * @param    $s		Name of timer
-   * @return splittime
-   *************/
+   * \brief splittime
+   * @param string $s - Name of timer
+   * @return mixed splittime
+   */
   function splittime($s) {
     $add = 0;
     if ($this->timers[$s]) {
-      list($usec_stop,  $sec_stop) = explode(" ", microtime());
+      list($usec_stop, $sec_stop) = explode(" ", microtime());
       list($usec_start, $sec_start) = explode(" ", $this->timers[$s]);
       $add = ((float)$usec_stop - (float)$usec_start) + (float)($sec_stop - $sec_start);
     }
-    return($this->sums[$s] + $add);
+    return ($this->sums[$s] + $add);
   }
 
   /**
    * \brief format
-   * @param    $format	name of default format (file, screen or perl);
-   *************/
+   * @param string $format - name of default format (file, screen or perl);
+   */
   function format($format) {
     if ($format == "perl") {
-      $this->prefix  = "{ 'url' => '" . urlencode($_SERVER["PHP_SELF"]) . "', 'ts' => " . time() .  ", ";
-      $this->delim   = ", ";
+      $this->prefix = "{ 'url' => '" . urlencode($_SERVER["PHP_SELF"]) . "', 'ts' => " . time() . ", ";
+      $this->delim = ", ";
       $this->postfix = " }";
-      $this->format  = "'%s' => %0.6f";
+      $this->format = "'%s' => %0.6f";
     }
     else if ($format == "file") {
       if (isset($_SERVER["REQUEST_URI"])) {
-        $this->prefix  = urlencode($_SERVER["REQUEST_URI"]) . ": ";
-      } else {
-        $this->prefix  = $_SERVER["PHP_SELF"] . ": ";
+        $this->prefix = urlencode($_SERVER["REQUEST_URI"]) . ": ";
       }
-      $this->delim   = " ";
+      else {
+        $this->prefix = $_SERVER["PHP_SELF"] . ": ";
+      }
+      $this->delim = " ";
       $this->postfix = "";
-      $this->format  = "%s => %0.6f";
+      $this->format = "%s => %0.6f";
     }
     else if ($format == "screen") {
       if (isset($_SERVER["REQUEST_URI"])) {
-        $this->prefix  = "<pre>\nTimings for: " . urlencode($_SERVER["REQUEST_URI"]) . ":\n";
-      } else {
-        $this->prefix  = "<pre>\nTimings for: " . $_SERVER["PHP_SELF"] . ":\n";
+        $this->prefix = "<pre>\nTimings for: " . urlencode($_SERVER["REQUEST_URI"]) . ":\n";
       }
-      $this->delim   = "\n";
+      else {
+        $this->prefix = "<pre>\nTimings for: " . $_SERVER["PHP_SELF"] . ":\n";
+      }
+      $this->delim = "\n";
       $this->postfix = "\n</pre>";
-      $this->format  = "%20s => %0.6f";
+      $this->format = "%20s => %0.6f";
     }
     else {
       die("FATAL: Unknown format in stopwatch");
@@ -151,31 +152,31 @@ class stopwatch {
   }
 
   /**
-   *  \brief dump all stoptimers
-   *  @param $delim		delimitor
-   *	@return Dump of timers;
-   *************/
+   * \brief dump all stoptimers
+   * @param mixed $delim delimitor
+   * @return string Dump of timers;
+   */
   function dump($delim = null) {
-    foreach($this->timers as $k => $v) {
+    foreach ($this->timers as $k => $v) {
       if (!is_null($v))
         $this->stop($k);
     }
 
-    $prefix  = $this->prefix;
+    $prefix = $this->prefix;
     $postfix = $this->postfix;
-    $format  = $this->format;
-    if (is_null($delim))   $delim   = $this->delim;				// Get delimitor or constructor delimitor
+    $format = $this->format;
+    if (is_null($delim)) $delim = $this->delim;        // Get delimitor or constructor delimitor
     // If unset: get defalut values
-    if (is_null($delim))   $delim   = "\n\t";
-    if (is_null($format))  $format  = "%s => %01.6f";
-    if (is_null($prefix))  $prefix  = "Timings for: " . $_SERVER['REQUEST_URI'] . (preg_match("/\n/", $delim) ? $delim : " ");
+    if (is_null($delim)) $delim = "\n\t";
+    if (is_null($format)) $format = "%s => %01.6f";
+    if (is_null($prefix)) $prefix = "Timings for: " . $_SERVER['REQUEST_URI'] . (preg_match("/\n/", $delim) ? $delim : " ");
     if (is_null($postfix)) $postfix = "\n";
-    if (!preg_match("/\n\$/", $postfix)) $postfix .= "\n";				// Make sure postfix ends in a newline
+    if (!preg_match("/\n\$/", $postfix)) $postfix .= "\n";        // Make sure postfix ends in a newline
 
-    $ret = Array();
+    $ret = [];
     //natcasesort($keys = array_keys($this->sums));
     $keys = array_keys($this->sums);
-    foreach($keys as $k) {
+    foreach ($keys as $k) {
       array_push($ret, sprintf($format, $k, $this->sums[$k]));
     }
     return $prefix . join($delim, $ret) . $postfix;
@@ -183,29 +184,29 @@ class stopwatch {
 
   /**
    * \brief log
-   *  @param   $file		filename to log in
-   *  @param   $logformat	format to use for log
-   *  @return	 BOOL;
-   *************/
+   * @param string $file filename to log in
+   * @param string $logformat format to use for log
+   * @return  boolean
+   */
   function log($file, $logformat = "perl") {
-    $prefix  = $this->prefix;							// Backup format
+    $prefix = $this->prefix;              // Backup format
     $postfix = $this->postfix;
-    $format  = $this->format;
-    $delim   = $this->delim;
+    $format = $this->format;
+    $delim = $this->delim;
 
-    if (! is_null($logformat))
+    if (!is_null($logformat))
       $this->format($logformat);
     if ($fd = fopen($file, "a")) {
       fwrite($fd, $this->dump());
       fclose($fd);
     }
 
-    $this->prefix  = $prefix;							// Restore format
+    $this->prefix = $prefix;              // Restore format
     $this->postfix = $postfix;
-    $this->format  = $format;
-    $this->delim   = $delim;
-    return ! !$fd;								// Boolize $fd
+    $this->format = $format;
+    $this->delim = $delim;
+    return !!$fd;                // Boolize $fd
   }
-};
+}
 
-?>
+;
